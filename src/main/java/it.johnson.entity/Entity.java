5112ee386b1aa698f0a1d107a82cf9232600f1f8
MukengeNJ <john.mukenge>
2023-04-28 15:19:42 +0200

    setting up and lifecycle callback

--- src/main/java/it.johnson.entity/Entity.java
+++ src/main/java/it.johnson.entity/Entity.java
@@ -1,2 +1,40 @@
-package it.johnson.entity;public class Entity {
+package it.johnson.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import java.time.LocalDate;
+
+/**
+ * Created by john.mukenge on 28/04/2023.
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@javax.persistence.Entity
+public class Entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String task;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+    private LocalDate dateCompleted;
+    private LocalDate dateCreated;
+
+    //before the entity be persisted into the database, the following method will be called --> lifecycle callbacks
+    @PrePersist
+    private void init() {
+        setDateCreated(LocalDate.now());
+    }
+
 }
